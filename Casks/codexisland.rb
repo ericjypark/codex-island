321633c80272200fa9ae12798cf38079dc5d3506
ericjypark <2019147551@yonsei.ac.kr>
2026-04-30 15:46:21 +0900

    chore(cask): update SHA-256 for v0.0.1 release

--- Casks/codexisland.rb
+++ Casks/codexisland.rb
@@ -1,6 +1,6 @@
 cask "codexisland" do
   version "0.0.1"
-  sha256 "REPLACE_AT_RELEASE_TIME"
+  sha256 "efd368b8b535528d1a6e5d837fe065f9c20088f664a41a8936828bb7f14bbef1"
 
   url "https://github.com/ericjypark/codex-island/releases/download/v#{version}/CodexIsland-#{version}.dmg"
   name "CodexIsland"
