f1670e385ef056aba59c7e09280a7324e55cbc60
ericjypark <2019147551@yonsei.ac.kr>
2026-04-30 16:02:05 +0900

    chore(cask): refresh SHA-256 after rebuild

--- Casks/codexisland.rb
+++ Casks/codexisland.rb
@@ -1,6 +1,6 @@
 cask "codexisland" do
   version "0.0.1"
-  sha256 "efd368b8b535528d1a6e5d837fe065f9c20088f664a41a8936828bb7f14bbef1"
+  sha256 "8ba2378eb4c620440ae129aba9bb03b0020f3411deac66481e002daee6cafcf5"
 
   url "https://github.com/ericjypark/codex-island/releases/download/v#{version}/CodexIsland-#{version}.dmg"
   name "CodexIsland"
